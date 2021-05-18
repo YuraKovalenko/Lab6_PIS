341bf2e43cd965ebe7eac842e289f052b9f1078b
YuraKovalenko <57789851+YuraKovalenko@users.noreply.github.com>
2021-05-18 19:55:43 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,11 +1,9 @@
 const http = require("http");
 
 http.createServer((request,  response) => {
-    if(request.url === "/is-92-008"){
+    
         response.end("Kovalenko Yura | 2 course | IS-92"); 
-    } else{
-        response.end("Error");
-    }
+   
 }).listen(3000, () => {
     console.log("Port 3000");
 });
